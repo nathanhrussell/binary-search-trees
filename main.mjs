033745133cf9ca33ccb777b8add586f2b243a9d5
nathanhrussell <173242953+nathanhrussell@users.noreply.github.com>
2025-03-27 08:59:34 +0100

    Populate buildTree

--- main.mjs
+++ main.mjs
@@ -13,6 +13,14 @@ class Tree {
     }
 
     buildTree(array) {
-        
+        if (array.length === 0) return null;
+
+        const midIndex = Math.floor(array.length / 2);
+        const root = new Node(array[midIndex]);
+
+        root.left = this.buildTree(array.slice(0, midIndex));
+        root.right = this.buildTree(array.slice(midIndex + 1));
+
+        return root;
     }
 }
